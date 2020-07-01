78de26556e2336ccd291cd5ae2f7891b7e396033
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-07-01 12:29:35 -0300

    http and json-convert

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(MaterialApp(
-    home: Container()),
+const request = "https://api.hgbrasil.com/finance?format=json&key=548c1152";
+
+void main() async {
+  http.Response response = await http.get(request);
+  json.decode(response.body);
+  runApp(
+    MaterialApp(home: Container()),
   );
 }
