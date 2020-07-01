ab5113aabcab13818931f45a0b5c79c109a8a3ec
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-07-01 15:37:04 -0300

    async

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=548c1152";
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]);
   runApp(
-    MaterialApp(home: Container()),
+    MaterialApp(home: Home()),
   );
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("BTC Converter"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
